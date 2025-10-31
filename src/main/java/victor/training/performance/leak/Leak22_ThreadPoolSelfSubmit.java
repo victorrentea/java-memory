452ca1ce91d256b2503bf4f0f0fff3aa8d51271d
Victor Rentea <victorrentea@gmail.com>
2025-10-31 18:29:46 +0200

    +tgread pool self-submit

--- src/main/java/victor/training/performance/leak/Leak22_ThreadPoolSelfSubmit.java
+++ src/main/java/victor/training/performance/leak/Leak22_ThreadPoolSelfSubmit.java
@@ -1,7 +1,6 @@
 package victor.training.performance.leak;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,43 +15,45 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static victor.training.performance.util.PerformanceUtil.sleepMillis;
+
 @Slf4j
 @RestController
 @RequestMapping("leak22")
 @RequiredArgsConstructor
-public class Leak22_ThreadLeakSelfSubmit {
+public class Leak22_ThreadPoolSelfSubmit {
   private final Confused confused;
   @GetMapping
-  public String hotEndpoint() throws InterruptedException, ExecutionException {
-    CompletableFuture<String> f1 = confused.async();
-    CompletableFuture<String> f2 = confused.async();
-    return f1.get() + f2.get();
+  public String deadlock() throws InterruptedException, ExecutionException {
+    return confused.asyncDeadlock().get(); // imagine 3 x
   }
 }
 @Service
 @RequiredArgsConstructor
+@Slf4j
 class Confused {
-  private final ThreadPoolTaskExecutor myExecutor;
+  private final ThreadPoolTaskExecutor executor3; // has 3 threads
 
-  @Async("myExecutor") //10 threaduri
-  public CompletableFuture<String> async() {
-    return CompletableFuture.supplyAsync(()->networkCall(),myExecutor);
+  @Async("executor3")
+  public CompletableFuture<String> asyncDeadlock() throws ExecutionException, InterruptedException {
+    sleepMillis(100); // calls can overlap
+    CompletableFuture<String> f1 = supplyAsync(()-> apiCall(), executor3);
+    CompletableFuture<String> f2 = supplyAsync(()-> apiCall(), executor3);
+    return CompletableFuture.completedFuture(f1.get() + f2.get());
   }
 
-  @SneakyThrows
-  public static String networkCall() {
-    Thread.sleep(1000);
-    return "data";
+  public static String apiCall() {
+    return "remote data";
   }
 }
 @Configuration
 @EnableAsync
 class Config {
   @Bean
-  ThreadPoolTaskExecutor myExecutor() {
+  ThreadPoolTaskExecutor executor3() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(10);
-    executor.setMaxPoolSize(10);
+    executor.setMaxPoolSize(3);
     executor.setQueueCapacity(100);
     executor.initialize();
     return executor;
